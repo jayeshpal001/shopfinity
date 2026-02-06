3e7849c8a81a643628eddbfacf981b5c92cf3580
jayeshpal001 <jayashpal5@gmail.com>
2026-02-06 12:15:16 +0530

    connection timeout added

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,14 +2,15 @@ const nodemailer = require("nodemailer");
 const asyncHandler = require("express-async-handler");
 
 const sendEmail = asyncHandler(async (to, subject, html) => {
-
   const transporter = nodemailer.createTransport({
     host: "smtp.sendgrid.net",
     port: 587,
+    secure: false,
     auth: {
       user: "apikey",
       pass: process.env.SENDGRID_API_KEY,
     },
+    connectionTimeout: 15000,
   });
 
   const mailOptions = {
