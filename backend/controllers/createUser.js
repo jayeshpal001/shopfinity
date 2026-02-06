9af090da6dcf379de3401dece49d9f56c94cf60b
jayeshpal001 <jayashpal5@gmail.com>
2026-02-06 12:03:58 +0530

    sendgrid added intead of gmail smtp

--- backend/controllers/createUser.js
+++ backend/controllers/createUser.js
@@ -1,44 +1,50 @@
-const User = require('../models/User');
-const TempUser = require('../models/TempUserSchema');
-const asyncHandler = require('express-async-handler');
-const sendEmail = require('../utils/sendEmail');
-const generateOtp = require('../utils/generateOtp');
-const OTPModel = require('../models/OTPModel');
-const bcrypt = require('bcrypt');
+const User = require("../models/User");
+const TempUser = require("../models/TempUserSchema");
+const asyncHandler = require("express-async-handler");
+const sendEmail = require("../utils/sendEmail");
+const generateOtp = require("../utils/generateOtp");
+const OTPModel = require("../models/OTPModel");
+const bcrypt = require("bcrypt");
 
 const createUser = asyncHandler(async (req, res) => {
-    const { name, email, password } = req.body;
+  const { name, email, password } = req.body;
 
-    if (!name || !email || !password) {
-        res.status(400);
-        throw new Error("All fields are required");
-    }
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
 
-    const isExists = await User.findOne({ email });
-    if (isExists) {
-        res.status(409);
-        throw new Error("Email already exist");
-    }
-    const hashPass = await bcrypt.hash(password, 10); 
-    await TempUser.create({name, email, password: hashPass})
+  const isExists = await User.findOne({ email });
+  if (isExists) {
+    res.status(409);
+    throw new Error("Email already exist");
+  }
+  const hashPass = await bcrypt.hash(password, 10);
+  await TempUser.create({ name, email, password: hashPass });
 
-    const otp = generateOtp(); 
-    const otpExpiry = Date.now()+5*60*1000; 
-    const hashOtp = await bcrypt.hash(otp, 10); 
+  const otp = generateOtp();
+  const otpExpiry = Date.now() + 5 * 60 * 1000;
+  const hashOtp = await bcrypt.hash(otp, 10);
 
-    await OTPModel.create({ email, otp: hashOtp, otpExpiry });
-    console.log(`OTP for ${email}: ${otp}`);
+  await OTPModel.create({ email, otp: hashOtp, otpExpiry });
+  console.log(`OTP for ${email}: ${otp}`);
 
-   await sendEmail(
-        email, 
-        "Email verification", 
-        `<h1>Hellow ${name} welcome to backend Your otp is ${otp} </h1>`
-    )
+  await sendEmail(
+    email,
+    "Email verification",
+    `
+<h2>Shopfinity Verification</h2>
+<p>Your OTP is:</p>
+<h1>${otp}</h1>
+<p>This code expires in 5 minutes.</p>
+<p>If you didn’t request this, ignore.</p>
+`,
+  );
 
-    res.status(201).json({
-        success: true,
-        message: "Registration successful. Please verify your email",
-    })
-})
+  res.status(201).json({
+    success: true,
+    message: "Registration successful. Please verify your email",
+  });
+});
 
-module.exports = createUser; 
+module.exports = createUser;
--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,32 +2,25 @@ const nodemailer = require("nodemailer");
 const asyncHandler = require("express-async-handler");
 
 const sendEmail = asyncHandler(async (to, subject, html) => {
-  if (!to || !subject || !html) {
-    throw new Error("Missing required email fields");
-  }
+
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
+    host: "smtp.sendgrid.net",
     port: 587,
-    secure: false,
     auth: {
-      user: process.env.EMAIL,
-      pass: process.env.PASS,
+      user: "apikey",
+      pass: process.env.SENDGRID_API_KEY,
     },
-    tls: {
-      rejectUnauthorized: false,
-    },
-    connectionTimeout: 10000,
   });
 
   const mailOptions = {
-    from: `"Shopfinity 👕" <${process.env.EMAIL}>`,
+    from: `Shopfinity <${process.env.EMAIL}>`,
     to,
     subject,
     html,
   };
 
   await transporter.sendMail(mailOptions);
-  console.log(" Email sent successfully to", to);
+  console.log("OTP sent to", to);
 });
 
 module.exports = sendEmail;
