6476f44eb707a8c093f81bf6d3a0b15a4a526785
jayeshpal001 <jayashpal5@gmail.com>
2026-02-06 12:23:16 +0530

    now intead of nodemailder i use sendgrid/mail

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,26 +1,18 @@
-const nodemailer = require("nodemailer");
+const sgMail = require("@sendgrid/mail");
 const asyncHandler = require("express-async-handler");
 
-const sendEmail = asyncHandler(async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    host: "smtp.sendgrid.net",
-    port: 587,
-    secure: false,
-    auth: {
-      user: "apikey",
-      pass: process.env.SENDGRID_API_KEY,
-    },
-    connectionTimeout: 15000,
-  });
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  const mailOptions = {
-    from: `Shopfinity <${process.env.EMAIL}>`,
+const sendEmail = asyncHandler(async (to, subject, html) => {
+
+  const msg = {
     to,
+    from: process.env.EMAIL, // verified sender
     subject,
     html,
   };
 
-  await transporter.sendMail(mailOptions);
+  await sgMail.send(msg);
   console.log("OTP sent to", to);
 });
 
